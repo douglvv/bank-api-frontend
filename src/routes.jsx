9359973015d8663d7040670286fa5975d82b5610
Douglas Vidal <vidaldl01@gmail.com>
2023-06-17 03:35:43 -0300

    update folder structure

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import HomeScreen from "./assets/screens/HomeScreen/HomeScreen";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 
 export default function AppRoutes(){
     return(
--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import Header from "../../components/header/Header";
+import Header from '../../components/header/Header';
 import React from 'react';
 
 export default function HomeScreen(props) {

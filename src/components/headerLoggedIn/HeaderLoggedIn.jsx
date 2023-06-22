7c9596c0ef38cb28d5126d40254ef7e46ea3ad30
Douglas Vidal <vidaldl01@gmail.com>
2023-06-22 10:45:41 -0300

    update header logged in

---
+++ src/components/headerLoggedIn/HeaderLoggedIn.jsx
@@ -0,0 +1,38 @@
+import { Container, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+import styles from './styles.module.css';
+import imgLogo from '/img/logo.png'
+import { BsPersonFillGear, BsBoxArrowRight, BsPersonCircle } from "react-icons/bs";
+
+export default function Header(props) {
+    return (
+        <>
+            <Navbar bg="light" expand="lg" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="/account">
+                        <img
+                            src={imgLogo}
+                            className={`${styles.imgBrand} d-inline-block align-top`}
+                            alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="navbar-nav">
+                        <Nav>
+                            <NavDropdown
+                                menuVariant="light"
+                                title={
+                                    <>
+                                        <BsPersonCircle /> {props.accountName}
+                                    </>
+                                }
+                            >
+                                <NavDropdown.Item href="#"><BsPersonFillGear /> Edit account</NavDropdown.Item>
+                                <NavDropdown.Item href="#"><BsBoxArrowRight /> Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
--- src/screens/AccountScreen/AccountScreen.jsx
+++ src/screens/AccountScreen/AccountScreen.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import HeaderLoggedIn from '../../components/headerLoggedIn/HeaderLoggedIn';
+import { useSelector } from "react-redux";
 
 export default function AccountScreen(){
-    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-    const token = useSelector((state) => state.auth.token);
     const account = useSelector((state) => state.auth.account);
-    
     return(
-        <div>
+        <>
+            <HeaderLoggedIn accountName={account.name} />
             <h1>Account Screen</h1>
-            <p>{JSON.stringify(isLoggedIn)}</p>
-            <p>{JSON.stringify(token)}</p>
-            <p>{JSON.stringify(account)}</p>
-        </div>
+        </>
     )
 }

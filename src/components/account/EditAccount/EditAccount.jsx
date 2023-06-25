a6058c4653818208e563ca0144ed67478ffee147
Douglas Vidal <vidaldl01@gmail.com>
2023-06-25 20:53:59 -0300

    conditional render editaccount

--- src/components/account/EditAccount/EditAccount.jsx
+++ src/components/account/EditAccount/EditAccount.jsx
@@ -1,31 +1,54 @@
-import React from "react";
-import { Container, Nav, Col, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import styles from './styles.module.css';
 import EditAccountForm from "../EditAccountForm/EditAccountForm";
+import EditPasswordForm from "../EditPasswordForm/EditPasswordForm";
 import { useDispatch } from "react-redux";
+import { logout } from "../../../auth/authSlice"
+import { BsBoxArrowRight, BsPersonCircle } from "react-icons/bs";
+import { RiLockPasswordLine } from "react-icons/ri";
+
 
 export default function EditAccount(props) {
+    const [activeKey, setActiveKey] = useState('personal')
     const dispatch = useDispatch();
 
     const handleLogout = () => {
         dispatch(logout());
     };
+
+    const renderContent = () => {
+        return activeKey === 'personal' ? (
+            <EditAccountForm account={props.account} />
+        ) : activeKey === 'password' ? (
+            <EditPasswordForm account={props.account} />
+        ) : null;
+    };
+
     return (
         <>
             <Container fluid="sm" className="d-flex justify-content-center align-items-center">
 
                 <Row as={'div'} className={`m-3 ${styles.wrapper}`}>
                     <h4 className="text-center">Account Settings</h4>
+
                     <Col sm={4} className={`${styles.wrapperNav} bg-dark text-white p-3`}>
                         <h5 className="text-center">{props.account.name}</h5>
-                        <Nav fill justify variant="underline" defaultActiveKey={'personal'} className="flex-sm-column">
-                            <Nav.Link className="text-white" eventKey={'personal'}>Personal Info</Nav.Link>
-                            <Nav.Link className="text-white" eventKey={'cancel'}>Password</Nav.Link>
-                            <Nav.Link className="text-white" eventKey={'logout'} onClick={handleLogout}>Logout</Nav.Link>
-                        </Nav>
+
+                        <div className="d-grid gap-2">
+                            <Button variant="secondary" onClick={() => setActiveKey("personal")}>
+                                <BsPersonCircle /> Personal Info
+                            </Button>
+                            <Button variant="secondary" onClick={() => setActiveKey("password")}>
+                                <RiLockPasswordLine /> Password
+                            </Button>
+                            <Button variant="secondary" onClick={handleLogout}>
+                                <BsBoxArrowRight /> Logout
+                            </Button>
+                        </div>
                     </Col>
                     <Col sm={8} className={`${styles.wrapperContent} bg-light text-orange p-3`}>
-                        <EditAccountForm account={props.account} />
+                        {renderContent()}
                     </Col>
                 </Row>
 
--- src/components/account/EditPasswordForm/EditPasswordForm.jsx
+++ src/components/account/EditPasswordForm/EditPasswordForm.jsx
@@ -1,15 +1,18 @@
 import { React, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row} from "react-bootstrap";
 import { Store as Notification } from 'react-notifications-component';
 import AccountService from '../../../services/accounts';
 import Loader from '../../loader/Loader'
 
-export default function EditAccountForm(props) {
-    const [name, setName] = useState();
-    const [cpf, setCpf] = useState();
+export default function EditPasswordForm(props) {
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [newPasswordRepeat, setNewPasswordRepeat] = useState("");
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [loading, setLoading] = useState(false);
+
+
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -46,29 +49,47 @@ export default function EditAccountForm(props) {
     return (
         <>
             <Container fluid="sm">
-            <h4 className="text-center">Edit Personal Info</h4>
+            <h4 className="text-center">Change Password</h4>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="name">
-                        <Form.Label>Name</Form.Label>
+                        <Form.Label>Current Password</Form.Label>
                         <Form.Control
-                            type="text"
-                            name="name"
+                            type="password"
+                            name="currentPassword"
                             placeholder={props.account.name}
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                            required
                         />
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="cpf">
-                        <Form.Label>CPF</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="cpf"
-                            placeholder={props.account.cpf}
-                            value={cpf}
-                            onChange={(e) => setCpf(e.target.value)}
-                        />
-                    </Form.Group>
+                    <Row>
+                        <Form.Group as={Col} className="mb-3" controlId="cpf">
+                            <Form.Label>New Password *</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="newPassword"
+                                placeholder={"Enter new password"}
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} className="mb-3" controlId="cpf">
+                            <Form.Label>New Password *</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="newPasswordRepeat"
+                                placeholder={"Repeat the new password"}
+                                value={newPasswordRepeat}
+                                onChange={(e) => setNewPasswordRepeat(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+                    </Row>
+
+
 
                     <div className="d-flex justify-content-center align-items-center">
                         <Button className="w-100" variant="orange" type="submit">

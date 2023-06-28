c200fb3f89f6339c6cc413df5f9dea5a96b4fb40
Douglas Vidal <vidaldl01@gmail.com>
2023-06-28 20:26:55 -0300

    delete comments

--- src/components/account/ShowAccount/ShowAccount.jsx
+++ src/components/account/ShowAccount/ShowAccount.jsx
@@ -1,4 +1,4 @@
-import { Container, Card, Row, Col, Button} from "react-bootstrap";
+import { Container, Row, Col, Button} from "react-bootstrap";
 import { useDispatch } from 'react-redux';
 import logout from '../../../auth/authSlice'
 import AccountBalanceCard from "../AccountBalanceCard/AccountBalanceCard";
@@ -13,17 +13,6 @@ export default function ShowAccount(props) {
     return (
         <>
             <Container fluid='sm'>
-                {/* <h3 className="mx-3">Account: {props.account._id}</h3>
-                <Row className="m-3">
-                    <Col sm={4}>
-                        <Card className="mb-3">
-                            <Card.Body>
-                                <Card.Title className="fs-2">$ {parseFloat(props.account.balance).toFixed(2)}</Card.Title>
-                                <Card.Subtitle className="fs-4 text-muted">Balance</Card.Subtitle>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row> */}
                 <AccountBalanceCard account={props.account} />
                 <hr />
                 <h3 className="mx-3">Actions</h3>

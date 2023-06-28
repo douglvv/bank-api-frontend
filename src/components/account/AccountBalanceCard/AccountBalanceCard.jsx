f9d3fe08f27e63618de9d9f250122424e052c5b0
Douglas Vidal <vidaldl01@gmail.com>
2023-06-28 18:18:14 -0300

    update

--- src/components/account/AccountBalanceCard/AccountBalanceCard.jsx
+++ src/components/account/AccountBalanceCard/AccountBalanceCard.jsx
@@ -12,7 +12,7 @@ export default function AccountBalanceCard(props) {
     const accountIdRef = useRef(null);
 
     return (
-        <Row as={'div'} className={`m-3 wrapperAccount`}>
+        <Row as={'div'}  className={`m-3 wrapperAccount`}>
             <h4 className="">Hello, {props.account.name}</h4>
 
             <Col sm={4} className={`wrapperNav bg-light text-orange p-3 `}>
@@ -24,21 +24,23 @@ export default function AccountBalanceCard(props) {
             <Col sm={8} className={`wrapperContent bg-dark text-white p-3`}>
                 <Stack gap={2}>
                     <Card bg="secondary" text="light">
-                        {/* <Card.Header>
-                            Account Number
-                        </Card.Header> */}
                         <Card.Body >
                             <div className="d-flex align-items-start justify-content-between">
                                 <div className="fw-bold">Account Number</div>
-                                <button onClick={() => clipboard.copy(accountIdRef.current.textContent)} title="Copy Account's Number" className={styles.iconButton}>
+
+                                <button
+                                    onClick={() => clipboard.copy(accountIdRef.current.textContent)}
+                                    title="Copy Account's Number"
+                                    className={styles.iconButton}
+                                >
                                     {clipboard.copied ? <FiCheck /> : <FiCopy />}
                                 </button>
+
                             </div>
                             <div>
                                 <p ref={accountIdRef}>
                                     {props.account._id}
                                 </p>
-
                             </div>
                         </Card.Body>
                     </Card>
